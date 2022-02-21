fde1b7e4e6a4472892727c315104bb065c6ef324
parkera24 <88694491+parkera24@users.noreply.github.com>
2022-02-21 10:21:18 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,179 @@
+from rembg.bg import remove
+import numpy as np
+import io
+from PIL import Image
+
+print('Starting...')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B1-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B1-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B2-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B2-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B3-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B3-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B4-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B4-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B5-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B5-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B6-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B6-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B7-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B7-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B8-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B8-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B9-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B9-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B11-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B11-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B13-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B13-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B14-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B14-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B15-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B15-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B16-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B16-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B17-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B17-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B19-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B19-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B20-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B20-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B21-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B21-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+input_path = '/Users/parkeraustin/Desktop/hb-b-png/B22-resized.png'
+output_path = '/Users/parkeraustin/Desktop/B-Done/B22-done.png'
+
+f = np.fromfile(input_path)
+result = remove(f)
+img = Image.open(io.BytesIO(result)).convert("RGBA")
+img.save(output_path)
+print('Poof Background Gone')
+
+print('Task Complete')
